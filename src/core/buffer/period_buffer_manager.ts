428c148c273e8328e4c2fabfac59ddf038aa9442
peaBerberian <pea.berberian@gmail.com>
2018-10-03 16:08:44 +0200

    api: add manualBitrateSwitchingMode option

--- src/core/buffer/period_buffer_manager.ts
+++ src/core/buffer/period_buffer_manager.ts
@@ -130,6 +130,7 @@ export default function PeriodBufferManager(
     segmentRetry? : number;
     offlineRetry? : number;
     textTrackOptions? : ITextTrackSourceBufferOptions;
+    manualBitrateSwitchingMode : "seamless"|"direct";
   }
 ) : Observable<IPeriodBufferManagerEvent> {
   const { manifest, initialPeriod } = content;
@@ -571,7 +572,8 @@ export default function PeriodBufferManager(
       pipeline,
       wantedBufferAhead$,
       { manifest, period, adaptation },
-      abrManager
+      abrManager,
+      options
     ).pipe(catchError((error : Error) => {
       // non native buffer should not impact the stability of the
       // player. ie: if a text buffer sends an error, we want to
